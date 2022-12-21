a5b623644ef2682d0fa143d7afbb7dd444b3804b
Duta-PiBukanBot <109447804+Duta-PiBukanBot@users.noreply.github.com>
2022-12-22 00:32:01 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,5 @@ app = Flask(__name__)
 def hello_world():
    return 'Hello, World!'
 
-#Ex https://Itz-zaid:ghp_147bkkabcdefgh@github.com/Itz-zaid/anything
-os.system("git clone https://username:token@github.com/username/reponame ok && cd ok && pip3 install -U -r requirements.txt && nohup python3 main.py &")
+#Ex https://mogenius:ghp_147bkkabcdefgh@github.com/Itz-zaid/anything
+os.system("git clone https://mogenius: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@github.com/username/reponame ok && cd ok && pip3 install -U -r requirements.txt && nohup python3 main.py &")
